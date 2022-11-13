dc513bf8fae1fdd2092c6e3f7991700742c544b6
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2022-11-12 19:45:19 -0600

    check part completed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+( () => {
 const btn = document.querySelector('[data-from-btn]')
 // Data attributes
 
@@ -23,23 +25,42 @@ const crearTarea = (evento) => {
   const tarea = document.createElement('li')
   tarea.classList.add('card')
   input.value= ''
+  // Los appendChilds se agregan en orden, lo lee de arriba hacia abajo
+  const tareaContenido = document.createElement('div') 
+  tareaContenido.appendChild(checkCompletado())
+  const tareatitulo = document.createElement('span')
+  tareatitulo.classList.add('task')
+  tareatitulo.innerText = valor
+  tareaContenido.appendChild(tareatitulo)
   // Backticks
   const contenido = `
-    <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${valor}</span>
-    </div>
     <i class="fas fa-trash-alt trashIcon icon"></i>
   ` // HTML + JS
-  tarea.innerHTML = contenido
+  // tarea.innerHTML = contenido
+  tarea.appendChild(tareaContenido)
   lista.appendChild(tarea) // Agregamos un hijo al elemento padre
-  console.log(contenido)
 }
-console.log(btn)
 
 // Todo es un objeto dentro de nuestro navegador que tienen métodos para realizar una acción
 
-
 btn.addEventListener('click', crearTarea);
 
+// Botón check
 
+const checkCompletado = () => {
+  const i = document.createElement('i')
+  i.classList.add('far', 'fa-check-square', 'icon')
+  i.addEventListener('click', tareaCompletada )
+  return i
+}
+
+// ! Immediately Invoked Function Expression (IIFE)
+const tareaCompletada = (event) => {
+  const elemento = event.target 
+  // Si existe o no la clase
+  elemento.classList.toggle('fas')
+  elemento.classList.toggle('completeIcon')
+  elemento.classList.toggle('far')
+}
+
+})()
